6978861e7bab9faf7511de0cac6bd4c07c555e62
Johel Llanos <merwinjlc@gmail.com>
2026-05-07 20:10:27 -0500

    sidebar

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Mi primera App con Python")
+
+st.sidebar.title("Parametros")
+
+nombre = st.sidebar.text_input("Introduce tu nombre")
+
+st.sidebar.success("Usuario identificado")
+
+st.write("¡Hola ", nombre, "!")
